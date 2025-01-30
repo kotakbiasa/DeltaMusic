f89a85e3d5ae9dcccb4cc27fd5ec1310ca565bc1
Kotak Biasa <muhnurfauzan73@gmail.com>
2025-01-30 21:00:07 +0800

    dev

--- DeltaMusic/plugins/devs/dev.py
+++ DeltaMusic/plugins/devs/dev.py
@@ -166,58 +166,34 @@ async def shellrunner(_, message: Message):
     if len(message.command) < 2:
         return await edit_or_reply(message, text="<b>Contoh :</b>\n/sh git pull")
     text = message.text.split(None, 1)[1]
-    if "\n" in text:
-        code = text.split("\n")
-        output = ""
-        for x in code:
-            shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
-            try:
-                process = subprocess.Popen(
-                    shell,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                )
-            except Exception as err:
-                await edit_or_reply(message, text=f"<b>ERROR :</b>\n<pre>{err}</pre>")
-            output += f"<b>{code}</b>\n"
-            output += process.stdout.read()[:-1].decode("utf-8")
-            output += "\n"
-    else:
-        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
-        for a in range(len(shell)):
-            shell[a] = shell[a].replace('"', "")
-        try:
-            process = subprocess.Popen(
-                shell,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-        except Exception as err:
-            print(err)
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
-            )
-            return await edit_or_reply(
-                message, text=f"<b>ERROR :</b>\n<pre>{''.join(errors)}</pre>"
-            )
-        output = process.stdout.read()[:-1].decode("utf-8")
-    if str(output) == "\n":
-        output = None
-    if output:
+    shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
+    for a in range(len(shell)):
+        shell[a] = shell[a].replace('"', "")
+    process = subprocess.Popen(
+        shell,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    output = ""
+    while True:
+        line = process.stdout.readline()
+        if not line:
+            break
+        output += line
         if len(output) > 4096:
-            with open("output.txt", "w+") as file:
-                file.write(output)
-            await app.send_document(
+            await app.send_message(
                 message.chat.id,
-                "output.txt",
+                f"<b>OUTPUT :</b>\n<pre>{output}</pre>",
                 reply_to_message_id=message.id,
-                caption="<code>Output</code>",
             )
-            return os.remove("output.txt")
-        await edit_or_reply(message, text=f"<b>OUTPUT :</b>\n<pre>{output}</pre>")
+            output = ""
+    if output:
+        await app.send_message(
+            message.chat.id,
+            f"<b>OUTPUT :</b>\n<pre>{output}</pre>",
+            reply_to_message_id=message.id,
+        )
     else:
         await edit_or_reply(message, text="<b>OUTPUT :</b>\n<code>None</code>")
     await message.stop_propagation()
